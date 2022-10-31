527ecd3ef9882c7704005d1dd4e7a7f02416b0ee
PreethiReddyA <116861430+PreethiReddyA@users.noreply.github.com>
2022-10-31 17:25:36 +0530

    Update Activity6.py

--- Python/Activities/Activity6.py
+++ Python/Activities/Activity6.py
@@ -1,3 +1,17 @@
+# Write a Python program to construct the following pattern, using a nested loop number.
+# 1
+# 22
+# 333
+# 4444
+# 55555
+# 666666
+# 7777777
+# 88888888
+# 999999999
+
+
+
+
 from re import I
 
 for i in range(10):
@@ -11,12 +25,3 @@ for i in range(10):
 
 
 
-# 1
-# 22
-# 333
-# 4444
-# 55555
-# 666666
-# 7777777
-# 88888888
-# 999999999
