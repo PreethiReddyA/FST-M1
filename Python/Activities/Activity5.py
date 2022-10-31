74c085e295c4259e5b195f3c3e2b460fc923b814
PreethiReddyA <116861430+PreethiReddyA@users.noreply.github.com>
2022-10-31 17:24:44 +0530

    Update Activity5.py

--- Python/Activities/Activity5.py
+++ Python/Activities/Activity5.py
@@ -1,3 +1,6 @@
+# Write a Python program to create the multiplication table (from 1 to 10) of a number.
+
+
 num=int(input("please enter any number: "))
 for i in range(1,11):
     print(num,'x',i,'=',num*i)
