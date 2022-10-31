6904964835e7557ccd2b280402b0846be3a6e02c
PreethiReddyA <116861430+PreethiReddyA@users.noreply.github.com>
2022-10-31 17:22:30 +0530

    Update Activity2.py

--- Python/Activities/Activity2.py
+++ Python/Activities/Activity2.py
@@ -1,3 +1,6 @@
+# Ask the user for a number.
+# Depending on what number they enter, tell them if the number is an even or odd number.
+
 number=int(input("please enter any number: "))
 if number%2==0:
     print("Given number is an even number")
