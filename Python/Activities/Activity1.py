5484d98b8d0fdfee9a400e9534b75f5d62e53a02
PreethiReddyA <116861430+PreethiReddyA@users.noreply.github.com>
2022-10-31 17:21:48 +0530

    Update Activity1.py

--- Python/Activities/Activity1.py
+++ Python/Activities/Activity1.py
@@ -1,3 +1,7 @@
+# Write a program that asks the user to enter their name and their age.
+# Print out a message addressed to them that tells them the year that they will turn 100 years old.
+
+
 UserName= input("please enter your name: ")
 Age= int(input("please enter your age: "))
 year = str( ( 2022 - Age ) + 100 )
