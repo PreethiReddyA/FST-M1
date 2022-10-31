1cb2edbd1d5793a9bd6b4fabb4fb2d0436ed370a
PreethiReddyA <116861430+PreethiReddyA@users.noreply.github.com>
2022-10-31 16:42:44 +0530

    Add files via upload

---
+++ Python/Activities/Activity1.py
@@ -0,0 +1,4 @@
+UserName= input("please enter your name: ")
+Age= int(input("please enter your age: "))
+year = str( ( 2022 - Age ) + 100 )
+print( UserName + " will be 100 years old in the year " + year )
